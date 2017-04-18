6c5dd1ebe4fb42ed9f7ab747680f207e023d984f
edumusiclife <edumusicplay@hotmail.com>
2017-04-18 16:15:21 +0200

    Finish Web, add button go top

--- js/main.js
+++ js/main.js
@@ -17,9 +17,8 @@
             });
             $(this).addClass('active');
             var target = this.hash;
-            $target = $(target);
             $('html, body').stop().animate({
-                'scrollTop': $target.offset().top + 2
+                'scrollTop': $(target).offset().top + 2
             }, 500, 'swing', function () {
                 window.location.hash = target;
                 $(document).on("scroll", onScroll);
